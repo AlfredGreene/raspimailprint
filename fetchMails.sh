baa63420ddfc7eb6fe50d627144c0d17a005b283
Sebastian Martens <sm@nonstatics.com>
2016-11-16 20:40:44 +0100

    fix rights

--- fetchMails.sh
+++ fetchMails.sh
@@ -1,1 +1,5 @@
-php mailImporter.php
+cd /home/pi/raspimailprint/
+
+# sudo is not nice but needs to be set to dialout via ttyACM0
+# sudo usermod -a -G dialout pi - is not working? (why? fix!)
+sudo php mailImporter.php
